b9fb485708c69eab47033b26bf9fdae5eb9e76c3
Glitch (stefans-creative-bots) <none>
2020-07-08 16:36:33 +0000

    🔦🎀 Updated with Glitch

---
+++ bots/raindotgifbot.js
@@ -0,0 +1,45 @@
+const helpers = require(__dirname + '/../helpers/helpers.js'),
+      generators = {
+        rain: require(__dirname + '/../generators/rain.js'),
+      },    
+      TwitterClient = require(__dirname + '/../helpers/twitter.js'),    
+      mastodonClient = require(__dirname + '/../helpers/mastodon.js'), 
+      tumblrClient = require(__dirname + '/../helpers/tumblr.js');
+
+const twitter = new TwitterClient( {
+  consumer_key: process.env.RAINDOTGIFBOT_TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.RAINDOTGIFBOT_TWITTER_CONSUMER_SECRET,
+  access_token: process.env.RAINDOTGIFBOT_TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.RAINDOTGIFBOT_TWITTER_ACCESS_TOKEN_SECRET
+} );
+
+const mastodon = new mastodonClient( {
+   access_token: process.env.RAINDOTGIFBOT_MASTODON_ACCESS_TOKEN,
+   api_url: process.env.RAINDOTGIFBOT_MASTODON_API
+} );
+
+const tumblr = new tumblrClient( {
+  tumblr_name: process.env.RAINDOTGIFBOT_TUMBLR_BLOG_NAME,  
+  consumer_key: process.env.RAINDOTGIFBOT_TUMBLR_CONSUMER_KEY,
+  consumer_secret: process.env.RAINDOTGIFBOT_TUMBLR_CONSUMER_SECRET,
+  token: process.env.RAINDOTGIFBOT_TUMBLR_CONSUMER_TOKEN,
+  token_secret: process.env.RAINDOTGIFBOT_TUMBLR_CONSUMER_TOKEN_SECRET
+} );
+
+module.exports = function(){
+  const statusText = helpers.randomFromArray([
+          '🌧️',
+          '🌧️🌧️',
+          '🌧️🌧️🌧️'
+        ]),
+        options = {
+          width: 640,
+          height: 480,
+        };
+
+  generators.rain( options, function( err, imageData ){
+    twitter.postImage( statusText, imageData );
+    mastodon.postImage( statusText, imageData );      
+    tumblr.postImage( statusText, imageData );        
+  } );
+};
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 /* Setting things up. */
 
+if ( !process.env.PROJECT_NAME || !process.env.PROJECT_ID ){
+  require( 'dotenv' ).config();
+}
+
 const path = require( 'path' ),
       express = require( 'express' ),
       app = require(__dirname + '/app.js'),
@@ -8,28 +12,43 @@ const path = require( 'path' ),
 
 const bots = [
   {
-    name: '@nycviewsbot',
-    function: require( __dirname + '/bots/nycviewsbot.js' ),
+    script: '/bots/nycviewsbot.js',
     interval: cronSchedules.EVERY_SIX_HOURS
   },
- {
-    name: '@southpoleviews',
-    function: require( __dirname + '/bots/southpoleviews.js' ),
+  {
+    script: '/bots/southpoleviews.js',
+    interval: cronSchedules.EVERY_SIX_HOURS
+  },
+  {
+    script: '/bots/raindotgifbot.js',
     interval: cronSchedules.EVERY_SIX_HOURS
   }
-  
 ];
 
 let nycviewsbot = require( __dirname + '/bots/nycviewsbot.js' );
 
 let listener = app.listen( process.env.PORT, function(){
-  console.log( `your bots are running on port ${ listener.address().port }` );
+  if ( bots && bots.length > 0 ){
+    console.log( `🤖 your bot${ bots.length === 1 ? ' is' : 's are' } running on port ${ listener.address().port }` );
 
-  bots.forEach( function( bot ){
-    console.log( `scheduling ${ bot.name } (${ bot.interval })...` );
-    ( new CronJob( bot.interval, function() {
-      bot.function();
-    } ) ).start();
-  } );  
-  
+    bots.forEach( function( bot ){
+      let botInterval;
+
+      for (const schedule in cronSchedules) {
+        if ( cronSchedules[schedule] === bot.interval ){
+          botInterval = schedule;
+        }
+      }
+
+      console.log( `🕒 scheduling ${ bot.script }: ${ botInterval }` );
+      const script = require( __dirname + bot.script );
+
+      ( new CronJob( bot.interval, function() {
+        script();
+      } ) ).start();
+    } );
+
+  } else {
+    console.log( '🚫 no bots to schedule' );
+  }
 } );

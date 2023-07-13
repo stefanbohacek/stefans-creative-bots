365bc92a1df8ad58ae466ee43f340b57d32af90b
Glitch (stefans-creative-bots) <none>
2023-07-13 04:43:17 +0000

    Added alternate text to @bearcam posts.

--- data/bearcams.js
+++ data/bearcams.js
@@ -1,74 +1,51 @@
 module.exports = [
   {
     name: "Brooks Falls Brown Bears",
+    description: "A short clip from a live webcam in Katmai National Park, Alaska. Typically shows a river with bears in it.",
     url: "https://explore.org/livecams/brown-bears/brown-bear-salmon-cam-brooks-falls",
-    webcam_url:
-      "https://explore.org/livecams/player/brown-bears/brown-bear-salmon-cam-brooks-falls",
-      youtube_url: "https://www.youtube.com/watch?v=HsLvnFQW_yM",
-      youtube_embed_url: "https://www.youtube.com/embed/HsLvnFQW_yM"
+    youtube_url: "https://www.youtube.com/watch?v=HsLvnFQW_yM",
+    tags: '#bearcam #KatmaiNationalPark #alaska #live #bears'
   },
   {
     name: "The Riffles Bear Cam",
+    description: "A short clip from a live webcam in Katmai National Park, Alaska. Typically shows a river with bears in it.",
     url: "https://explore.org/livecams/brown-bears/brown-bear-salmon-cam-the-riffles",
-    webcam_url:
-      "https://explore.org/livecams/player/brown-bears/brown-bear-salmon-cam-the-riffles",
-      youtube_url: "https://www.youtube.com/watch?v=-VGfAjPsmHA",
-      youtube_embed_url: "https://www.youtube.com/embed/-VGfAjPsmHA"
+    youtube_url: "https://www.youtube.com/watch?v=-VGfAjPsmHA",
+    tags: '#bearcam #KatmaiNationalPark #alaska #live #bears'
   },
   {
     name: "Brooks Falls Brown Bears Low",
+    description: "A short clip from a live webcam in Katmai National Park, Alaska. Typically shows a river with bears in it.",
     url: "https://explore.org/livecams/brown-bears/brooks-falls-brown-bears-low",
-    webcam_url:
-      "https://explore.org/livecams/player/brown-bears/brooks-falls-brown-bears-low",
-      youtube_url: "https://www.youtube.com/watch?v=b6HFyBmGx28",
-      youtube_embed_url: "https://www.youtube.com/embed/b6HFyBmGx28"
+    youtube_url: "https://www.youtube.com/watch?v=b6HFyBmGx28",
+    tags: '#bearcam #KatmaiNationalPark #alaska #live #bears'
   },
   {
     name: "Kat's River View",
+    description: "A short clip from a live webcam in Katmai National Park, Alaska. Typically shows a river with bears in it.",
     url: "https://explore.org/livecams/brown-bears/brown-bear-salmon-cam-lower-river",
-    webcam_url:
-      "https://explore.org/livecams/player/brown-bears/brown-bear-salmon-cam-lower-river",
-      youtube_url: "https://www.youtube.com/watch?v=nzzBwdxfpqo",
-      youtube_embed_url: "https://www.youtube.com/embed/nzzBwdxfpqo"
+    youtube_url: "https://www.youtube.com/watch?v=nzzBwdxfpqo",
+    tags: '#bearcam #KatmaiNationalPark #alaska #live #bears'
   },
   {
     name: "River Watch Bear Cam",
+    description: "A short clip from a live webcam in Katmai National Park, Alaska. Typically shows a river with bears in it.",
     url: "https://explore.org/livecams/brown-bears/river-watch-brown-bear-salmon-cams",
-    webcam_url:
-      "https://explore.org/livecams/player/brown-bears/river-watch-brown-bear-salmon-cams",
-      youtube_url: "https://www.youtube.com/watch?v=Ly4v1lMvyPk",
-      youtube_embed_url: "https://www.youtube.com/embed/Ly4v1lMvyPk"
+    youtube_url: "https://www.youtube.com/watch?v=Ly4v1lMvyPk",
+    tags: '#bearcam #KatmaiNationalPark #alaska #live #bears'
   },
   {
     name: "Dumpling Mountain - Katmai National Park",
+    description: "A short clip from a live webcam in Katmai National Park, Alaska. Typically shows a view from a mountain.",
     url: "https://explore.org/livecams/brown-bears/dumpling-mountain-brown-bear-salmon-cams",
-    webcam_url:
-      "https://explore.org/livecams/player/zen-den/dumpling-mountain-brown-bear-salmon-cams",
-      youtube_url: "https://www.youtube.com/watch?v=dXp23eXXTOU",
-      youtube_embed_url: "https://www.youtube.com/embed/dXp23eXXTOU"
+    youtube_url: "https://www.youtube.com/watch?v=dXp23eXXTOU",
+    tags: '#bearcam #KatmaiNationalPark #alaska #live'
   },
   {
     name: "Naknek River",
+    description: "A short clip with highlights from a webcam near a river in Katmai National Park, Alaska.",
     url: "https://explore.org/livecams/brown-bears/alaska-naknek-river",
-    webcam_url:
-      "https://explore.org/livecams/player/brown-bears/alaska-naknek-river",
-      youtube_url: "https://www.youtube.com/watch?v=7B8ArsmXPyk",
-      youtube_embed_url: "https://www.youtube.com/embed/7B8ArsmXPyk"
-  },
-  {
-    name: "Brown Bears of Katmai Alaska Meditation",
-    url: "https://explore.org/livecams/brown-bears/brown-bears-meditation",
-    webcam_url:
-      "https://explore.org/livecams/player/brown-bears/brown-bears-meditation",
-      youtube_url: "https://www.youtube.com/watch?v=LuImCh7wL2I",
-      youtube_embed_url: "https://www.youtube.com/embed/LuImCh7wL2I"
-  },
-  {
-    name: "Brooks Live Chat",
-    url: "https://explore.org/livecams/brown-bears/brooks-live-chat",
-    webcam_url:
-      "https://explore.org/livecams/player/brown-bears/brooks-live-chat",
-      youtube_url: "https://www.youtube.com/watch?v=SUuaijonOfk",
-      youtube_embed_url: "https://www.youtube.com/embed/SUuaijonOfk"
-  },
+    youtube_url: "https://www.youtube.com/watch?v=7B8ArsmXPyk",
+    tags: '#bearcam #KatmaiNationalPark #alaska'
+  }
 ];

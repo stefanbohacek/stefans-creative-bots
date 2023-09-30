6cd671d71cebe510734294d3cae5bd967a6e4ba2
Glitch (stefans-creative-bots) <none>
2023-09-30 18:17:55 +0000

    Minor cleanup.

--- bots/maps/bot.js
+++ bots/maps/bot.js
@@ -1,45 +1,52 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 import mastodonClient from "./../../modules/mastodon/index.js";
 import downloadFile from "./../../modules/download-file.js";
 
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const botID = '@maps';
+const botID = "@maps";
 
 const botScript = async () => {
   const mastodon = new mastodonClient({
     access_token: process.env.MAPS_BOT_ACCESS_TOKEN_SECRET,
     api_url: process.env.MAPS_BOT_API,
   });
-  
-  const response = await fetch('https://www.davidrumsey.com/luna/servlet/as/fetchMediaSearch?&lc=RUMSEY~8~1&q=what=Atlas+Map&bs=1&fullData=true&random=true');
+
+  const response = await fetch(
+    "https://www.davidrumsey.com/luna/servlet/as/fetchMediaSearch?&lc=RUMSEY~8~1&q=what=Atlas+Map&bs=1&fullData=true&random=true"
+  );
   let map = await response.json();
   map = map[0];
-  
+
   console.log("openig a map...");
 
-  const imageURL = map.urlSize4 || map.urlSize3 || map.urlSize2 || map.urlSize1 || map.urlSize0;
-  
-  console.log({imageURL});
+  const imageURL =
+    map.urlSize4 ||
+    map.urlSize3 ||
+    map.urlSize2 ||
+    map.urlSize1 ||
+    map.urlSize0;
 
-  if (imageURL){
+  console.log({ imageURL });
+
+  if (imageURL) {
     try {
       const attributes = JSON.parse(map.attributes);
       const filePath = `${__dirname}/../../temp/${botID}`;
       await downloadFile(imageURL, filePath);
 
-      const source = `https://www.davidrumsey.com/luna/servlet/detail/${map.id}`
-      const status = `${attributes.full_title}: ${source}\n\n#map #maps #HistoricalMaps`;
-      const description = `${attributes.type}: ${attributes.pub_note}`;
-    
+      const source = `https://www.davidrumsey.com/luna/servlet/detail/${map.id}`;
+      const status = `${attributes.full_title} ${source}\n\n#map #maps #HistoricalMaps`;
+      const description = attributes.pub_note || ""
+
       mastodon.postImage({
         status,
         image: filePath,
-        alt_text: description
+        alt_text: description,
       });
     } catch (error) {
       console.log("maps error", error);

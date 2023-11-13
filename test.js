353cd6699528587b2d9e97671d17c9210a40a741
Glitch (stefans-creative-bots) <none>
2023-11-13 21:06:10 +0000

    Cleanup.

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-// import bot from './bots/wikipediatopedits/bot.js';
-import bot from './bots/thelick/bot.js';
+import bot from './bots/wikipediatopedits/bot.js';
+//import bot from './bots/thelick/bot.js';
 bot();

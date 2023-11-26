08142efa7e088de44f209b1ab6056bf41706e1f9
Glitch (stefans-creative-bots) <none>
2023-11-26 13:54:07 +0000

    Added lighthouses.

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import bot from "./bots/test/bot.js";
+import bot from "./bots/lighthouses/bot.js";
+// import bot from "./bots/test/bot.js";
 // import bot from './bots/wikipediatopedits/bot.js';
 
 bot();

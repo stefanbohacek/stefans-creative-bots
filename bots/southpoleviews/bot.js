84937062dceae88fcaa95b0426e11a45bb5c17b1
Glitch (stefans-creative-bots) <none>
2023-11-29 14:16:14 +0000

    Updated list of stations.

--- bots/southpoleviews/bot.js
+++ bots/southpoleviews/bot.js
@@ -8,6 +8,7 @@ import getWeather from "./../../modules/get-weather.js";
 
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { stat } from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -20,46 +21,55 @@ const botScript = async () => {
       access_token: process.env.SOUTHPOLEVIEWSBOT_MASTODON_ACCESS_TOKEN,
       api_url: process.env.SOUTHPOLEVIEWSBOT_MASTODON_API,
     });
-
+    
     const station = randomFromArray(stations);
+    // const station = randomFromArray(stations.filter(s => s.name === "Troll research station"));
     const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
-
-    process.on("unhandledRejection", (reason, p) => {
-      console.error("Unhandled Rejection at: Promise", p, "reason:", reason);
-      browser.close();
-    });
-
-    const page = await browser.newPage();
-    page.setUserAgent(
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
-    );
-
-    await page.setDefaultNavigationTimeout(120000);
-
-    await page.goto(station.url, {
-      // waitUntil: "networkidle0",
-      waitUntil: "domcontentloaded",
-      timeout: 120000,
-    });
-
-    await page.waitForSelector(station.element, { timeout: 120000 });
-
-    const imageElement = await page.$(station.element);
-
-    const image = await page.evaluate(
-      (imageElement) => imageElement.getAttribute("src"),
-      imageElement
-    );
-
     let imageURL;
 
-    if (image) {
-      if (image.indexOf("http") === -1) {
-        imageURL = `${station.page_url}${image}`;
+    if (station.image_url){
+      imageURL = station.image_url;
+    } else {
+      process.on("unhandledRejection", (reason, p) => {
+        console.error("Unhandled Rejection at: Promise", p, "reason:", reason);
+        browser.close();
+      });
+  
+      const page = await browser.newPage();
+      page.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+      );
+  
+      await page.setDefaultNavigationTimeout(120000);
+  
+      await page.goto(station.url, {
+        // waitUntil: "networkidle0",
+        waitUntil: "domcontentloaded",
+        timeout: 120000,
+      });
+  
+      await page.waitForSelector(station.element, { timeout: 120000 });
+  
+      const imageElement = await page.$(station.element);
+  
+      const image = await page.evaluate(
+        (imageElement) => imageElement.getAttribute("src"),
+        imageElement
+      );
+  
+      if (image) {
+        if (image.indexOf("http") === -1) {
+          imageURL = `${station.page_url}${image}`;
+        } else {
+          imageURL = image;
+        }
       } else {
-        imageURL = image;
+        console.log("@southpoleviews error: image element not found", station);
       }
+      await browser.close();
+    }
 
+    if (imageURL){
       const filePath = `${__dirname}/../../temp/${botID}.jpg`;
       await downloadFile(imageURL, filePath);
 
@@ -87,12 +97,11 @@ const botScript = async () => {
       } else {
         console.log('@southpoleviews: image too dark, retrying...');
         await botScript();
-      }
+      }      
     } else {
-      console.log("@southpoleviews error: image element not found", station);
+      console.log('@southpoleviews: image not found, retrying...');
+      await botScript();
     }
-
-    await browser.close();
   })();
 };
 
--- data/webcams/south-pole-stations.js
+++ data/webcams/south-pole-stations.js
@@ -3,116 +3,77 @@ Some sites don't use full URL for the img src attribute and need page_url to be 
 */
 export default [
   {
+    name: "Troll research station",
+    image_url: "https://trollcam.npolar.no/TrollWebCam1HD.jpg",
+    url: "https://www.npolar.no/en/troll/webcam/",
+  },
+  {
+    name: "Troll research station",
+    image_url: "https://trollcam.npolar.no/TrollWebCam3HD.jpg",
+    url: "https://www.npolar.no/en/troll/webcam/",
+  },
+  {
     name: "Amundsen-Scott South Pole station",
     url: "https://www.usap.gov/videoclipsandmaps/spWebCam.cfm",
     page_url: "https://www.usap.gov/videoclipsandmaps/",
     element: "#img-aro",
-    location: {
-      lat: -90,
-      lon: 0,
-    },
   },
   {
     name: "Palmer station",
     url: "https://www.usap.gov/videoclipsandmaps/palwebcam.cfm?t=0",
     page_url: "https://www.usap.gov/videoclipsandmaps/",
     element: "#img-palmer",
-    location: {
-      lat: 64.7743,
-      lon: 64.0538,
-    },
   },
-
   {
     name: "Casey station",
     url: "https://www.antarctica.gov.au/antarctic-operations/webcams/casey/",
     element: "figure.featured img",
-    location: {
-      lat: 66.2821,
-      lon: 110.5285,
-    },
   },
   {
     name: "Davis station",
     url: "https://www.antarctica.gov.au/antarctic-operations/webcams/davis/",
     element: "figure.featured img",
-    location: {
-      lat: 68.5762,
-      lon: 77.9696,
-    },
   },
   {
     name: "Macquarie Island station",
     url: "https://www.antarctica.gov.au/antarctic-operations/webcams/macquarie-island/",
     element: "figure.featured img",
-    location: {
-      lat: 54.4997,
-      lon: 158.9364,
-    },
   },
   {
     name: "Mawson station",
     url: "https://www.antarctica.gov.au/antarctic-operations/webcams/mawson/",
     element: "figure.featured img",
-    location: {
-      lat: 67.6033,
-      lon: 62.8742,
-    },
   },
   {
     name: "Bird Island research station",
     url: "https://www.bas.ac.uk/data/our-data/images/webcams/bird-island-webcam/",
     element: '.entry-content img[width="640"]',
-    location: {
-      lat: 54.0084,
-      lon: 38.0513,
-    },
   },
   {
     name: "King Edward Point",
     url: "https://www.bas.ac.uk/data/our-data/images/webcams/king-edward-point-webcam/",
     element: '.entry-content img[width="640"]',
-    location: {
-      lat: 54.2833,
-      lon: 36.4937,
-    },
   },
-// posts old images
-//   {
-//     name: "RRS James Clark Ross",
-//     url: "https://www.bas.ac.uk/data/our-data/images/webcams/rrs-james-clark-ross-webcam/",
-//     element: '.entry-content img[width="640"]',
-//     location: {
-//       lat: 82.8628,
-//       lon: 135.0,
-//     },
-//   },
+  // posts old images
+  //   {
+  //     name: "RRS James Clark Ross",
+  //     url: "https://www.bas.ac.uk/data/our-data/images/webcams/rrs-james-clark-ross-webcam/",
+  //     element: '.entry-content img[width="640"]',
+  //   },
   {
     name: "RRS Sir David Attenborough Webcam",
     url: "https://www.bas.ac.uk/data/our-data/images/webcams/rrs-sir-david-attenborough-webcam/",
     element: '.entry-content img[width="1920"]',
-    location: {
-      lat: 82.8628,
-      lon: 135.0,
-    },
   },
   {
     name: "King Edward Point webcam",
     url: "https://www.bas.ac.uk/data/our-data/images/webcams/king-edward-point-webcam/",
     element: '.entry-content img[width="640"]',
-    location: {
-      lat: 54.2833,
-      lon: 36.4937,
-    },
   },
   {
     name: "Rothera webcam",
     url: "https://www.bas.ac.uk/data/our-data/images/webcams/rothera-webcam/",
     element: '.entry-content img[width="640"]',
-    location: {
-      lat: 67.5678,
-      lon: 68.1267,
-    },
   },
   // {
   //   name: "Halley VI webcam",

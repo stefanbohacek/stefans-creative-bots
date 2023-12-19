15355e996e93b764f8ad334b681ea5ee1900cf44
Glitch (stefans-creative-bots) <none>
2023-12-19 13:24:19 +0000

    Updated list of train videos.

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import bot from "./bots/vzvzvz/bot.js";
+//import bot from "./bots/vzvzvz/bot.js";
 // import bot from "./bots/threadsfederated/bot.js";
-// import bot from './bots/wikipediatopedits/bot.js';
+import bot from './bots/wikipediatopedits/bot.js';
 
 bot();

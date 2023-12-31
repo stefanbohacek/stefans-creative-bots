48c66ecf319253dcfbe65a7a99f823a1bee2d9f4
Glitch (stefans-creative-bots) <none>
2023-12-31 21:03:04 +0000

    Work in progress: year progress bot.

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-// import bot from "./bots/test/bot.js";
-import bot from "./bots/exoplanets/bot.js";
+import bot from "./bots/test/bot.js";
+// import bot from "./bots/exoplanets/bot.js";
 // import bot from './bots/wikipediatopedits/bot.js';
 
 bot();

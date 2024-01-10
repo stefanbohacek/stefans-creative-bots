6047808f76cd5103dfedd766bacbf4c9f7aa15be
Glitch (stefans-creative-bots) <none>
2024-01-10 15:12:54 +0000

    Added vibecheck bot.

---
+++ bots/vibecheck/bot.js
@@ -0,0 +1,15 @@
+import mastodonClient from "./../../modules/mastodon/index.js";
+
+const botScript = async () => {
+  const mastodon = new mastodonClient({
+    access_token: process.env.VIBECHECK_BOT_MASTODON_TOKEN,
+    api_url: process.env.BOTSINSPACE_API_URL,
+  });
+
+  const options = ["Great!", "Could be better."];
+  const status = "How's your day going? #vibe #vibecheck #poll";
+  console.log({ status, options });
+  mastodon.postPoll(status, options);
+};
+
+export default botScript;

a9c61f23548ff6d940c11ee1847aa9856c3574b1
Glitch (stefans-creative-bots) <none>
2024-02-14 12:51:48 +0000

    Cleanup.

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 // import bot from "./bots/test/bot.js";
 // import bot from "./bots/thisdatasetdoesnotexist/bot.js";
-import bot from "./bots/wyrf_bot/bot.js";
+import bot from "./bots/lighthouses/bot.js";
 // import bot from './bots/wikipediatopedits/bot.js';
 
 bot();

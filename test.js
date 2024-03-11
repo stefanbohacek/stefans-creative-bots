4d6d55bea6205ac02f36498fcec250a15fa65f24
Glitch (stefans-creative-bots) <none>
2024-03-11 18:44:56 +0000

    Updated image descriptions for maps bot.

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 // import bot from "./bots/test/bot.js";
 // import bot from "./bots/thisdatasetdoesnotexist/bot.js";
-import bot from "./bots/mastodon_mobile_apps/bot.js";
+import bot from "./bots/maps/bot.js";
 // import bot from './bots/wikipediatopedits/bot.js';
 
 bot();

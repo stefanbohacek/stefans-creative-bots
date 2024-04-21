90a5592a733bb8c226b28b2b1a98e49646fc077c
Glitch (stefans-creative-bots) <none>
2024-04-21 21:06:57 +0000

    Added Lakes bot.

---
+++ bots/lakes/bot.js
@@ -0,0 +1,45 @@
+import webcams from "./../../data/webcams/lakes.js";
+import mastodonClient from "./../../modules/mastodon/index.js";
+
+import downloadFile from "./../../modules/download-file.js";
+import randomFromArray from "./../../modules/random-from-array.js";
+import getWeather from "./../../modules/get-weather.js";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const botID = "lakes";
+
+const botScript = async () => {
+  const mastodon = new mastodonClient({
+    access_token: process.env.LAKES_BOT_MASTODON_ACCESS_TOKEN,
+    api_url: process.env.BOTSINSPACE_API_URL,
+  });
+
+  const webcam = randomFromArray(webcams);
+  const webcamUrl = `📷 ${webcam.link}`;
+
+  const filePath = `${__dirname}/../../temp/${botID}.jpg`;
+  await downloadFile(webcam.url, filePath);
+
+  const mapURL = `🗺️ https://www.openstreetmap.org/?mlat=${webcam.latitude}&mlon=${webcam.longitude}#map=12/${webcam.latitude}/${webcam.longitude}`;
+  const weather = await getWeather(webcam.latitude, webcam.longitude);
+  const status = `${webcam.title}\n${webcamUrl}\n${mapURL} #nyc #webcam #city`;
+  let description = webcam.description;
+
+  if (weather && weather.description_full) {
+    description += ` ${weather.description_full}`;
+  }
+
+  mastodon.postImage({
+    status,
+    image: filePath,
+    alt_text: description,
+  });
+  return true;
+};
+
+export default botScript;

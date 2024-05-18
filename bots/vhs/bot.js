bbefee347b79d1e30dd0ef5c8eca36a0e8c43a0f
Glitch (stefans-creative-bots) <none>
2024-05-18 03:43:38 +0000

    🕦🐄 Updated with Glitch

---
+++ bots/vhs/bot.js
@@ -0,0 +1,62 @@
+import fs from "fs";
+import mastodonClient from "./../../modules/mastodon/index.js";
+import webcams from "./../../data/webcams/vhs.js";
+import extractVideoLive from "./../../modules/extract-video-live.js";
+import downloadFile from "./../../modules/download-file.js";
+import getRandomInt from "./../../modules/get-random-int.js";
+import randomFromArray from "./../../modules/random-from-array.js";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const botID = "vhs";
+
+const botScript = async () => {
+  await (async () => {
+    try {
+      const mastodon = new mastodonClient({
+        access_token: process.env.VHS_BOT_MASTODON_ACCESS_TOKEN,
+        api_url: process.env.BOTSINSPACE_API_URL,
+      });
+
+      const webcam = randomFromArray(webcams);
+      const status = `${webcam.youtube_url}\n\n${webcam.tags}`;
+
+      if ("video_start" in webcam && "video_end" in webcam) {
+        const url = `https://tools.stefanbohacek.dev/video-dl/?platform=youtube&id=${
+          webcam.youtube_id
+        }&start=${getRandomInt(
+          webcam.video_start,
+          webcam.video_end - 10
+        )}&length=10&token=${process.env.STEFANS_TOOLS_ACCESS_TOKEN}`;
+        console.log(url);
+        await downloadFile(url, __dirname + `/../../temp/${botID}.mp4`);
+        try {
+          fs.renameSync(
+            __dirname + `/../../temp/${botID}.mp4.mkv`,
+            __dirname + `/../../temp/${botID}.mp4`
+          );
+        } catch (err) {
+          /* noop */
+        }
+      } else {
+        await extractVideoLive(webcam.youtube_url, `${botID}.mp4`, 10);
+      }
+      
+      console.log({status, alt_text: webcam.description});
+
+      mastodon.postImage({
+        status,
+        image: __dirname + `/../../temp/${botID}.mp4`,
+        alt_text: webcam.description,
+      });
+    } catch (error) {
+      console.log(`${botID} error`, error);
+    }
+  })();
+};
+
+export default botScript;
---
+++ data/webcams/vhs.js
@@ -0,0 +1,106 @@
+export default [
+  {
+    description:
+      "A short clip of an analog VHS noise, mostly a dark screen with some random visual artifacts occasionally popping in.",
+    youtube_url: "https://www.youtube.com/watch?v=cDxw8Pw7B9w",
+    youtube_id: "cDxw8Pw7B9w",
+    tags: "#vhs #analog",
+    video_start: 16,
+    video_end: 35350,
+  },
+  {
+    description:
+      "A short clip of an analog VHS noise with strong visual artifacts.",
+    youtube_url: "https://www.youtube.com/watch?v=VdLM5uf2O8Q",
+    youtube_id: "VdLM5uf2O8Q",
+    tags: "#vhs #analog",
+    video_start: 0,
+    video_end: 20,
+  },
+  {
+    description:
+      "A short clip of an analog VHS noise, mostly a dark screen with some random visual artifacts occasionally popping in.",
+    youtube_url: "https://www.youtube.com/watch?v=qELSSAspRDI",
+    youtube_id: "qELSSAspRDI",
+    tags: "#vhs #analog",
+    video_start: 0,
+    video_end: 2385,
+  },
+  {
+    description:
+      "A short clip from a VHS tape, showing a blue screen with white text overlay, various timestamps, date, or menu items.",
+    youtube_url: "https://www.youtube.com/watch?v=Ir6BVFIH1uY",
+    youtube_id: "Ir6BVFIH1uY",
+    tags: "#vhs #analog",
+    video_start: 143,
+    video_end: 316,
+  },
+  {
+    description:
+      "A short clip of an analog VHS noise, mostly a dark screen with some random visual artifacts occasionally popping in.",
+    youtube_url: "https://www.youtube.com/watch?v=hO12hl1eKOo",
+    youtube_id: "hO12hl1eKOo",
+    tags: "#vhs #analog",
+    video_start: 0,
+    video_end: 288,
+  },
+  {
+    description:
+      "A short clip of an analog VHS noise, mostly a dark screen with some random visual artifacts occasionally popping in.",
+    youtube_url: "https://www.youtube.com/watch?v=tf8BfnMoafk",
+    youtube_id: "tf8BfnMoafk",
+    tags: "#vhs #analog",
+    video_start: 0,
+    video_end: 64,
+  },
+  {
+    description: "A short clip of black and white static noise on TV.",
+    youtube_url: "https://www.youtube.com/watch?v=vTGcaK0LOto",
+    youtube_id: "vTGcaK0LOto",
+    tags: "#vhs #analog",
+    video_start: 0,
+    video_end: 45,
+  },
+  {
+    description:
+      "A short clip of an analog VHS noise, mostly a dark screen with some random visual artifacts occasionally popping in.",
+    youtube_url: "https://www.youtube.com/watch?v=8tZ_N3wP9Ko",
+    youtube_id: "8tZ_N3wP9Ko",
+    tags: "#vhs #analog",
+    video_start: 0,
+    video_end: 200,
+  },
+  {
+    description:
+      "A short clip of an analog VHS noise, mostly a dark screen with some random visual artifacts occasionally popping in.",
+    youtube_url: "https://www.youtube.com/watch?v=qIeOs3WdWvY",
+    youtube_id: "qIeOs3WdWvY",
+    tags: "#vhs #analog",
+    video_start: 0,
+    video_end: 810,
+  },
+  {
+    description: "DESCRIPTION",
+    youtube_url: "https://www.youtube.com/watch?v=cmHErUOlxCA",
+    youtube_id: "cmHErUOlxCA",
+    tags: "#vhs #analog",
+    video_start: 0,
+    video_end: 130,
+  },
+  // {
+  //   description: "DESCRIPTION",
+  //   youtube_url: "YT_URL",
+  //   youtube_id: "YT_ID",
+  //   tags: "#vhs #analog",
+  //   video_start: 1000,
+  //   video_end: 2000,
+  // },
+  // {
+  //   description: "DESCRIPTION",
+  //   youtube_url: "YT_URL",
+  //   youtube_id: "YT_ID",
+  //   tags: "#vhs #analog",
+  //   video_start: 1000,
+  //   video_end: 2000,
+  // },
+];

b8023ce502bb4f36bb6341ff6c2651a05e4e4f1e
Glitch (stefans-creative-bots) <none>
2024-06-02 05:09:56 +0000

    Check image luminosity before posting.

--- bots/volcanoviews/bot.js
+++ bots/volcanoviews/bot.js
@@ -1,17 +1,17 @@
 import webcams from "./../../data/webcams/volcanoes.js";
 import mastodonClient from "./../../modules/mastodon/index.js";
-
-import downloadFile from './../../modules/download-file.js';
+import downloadFile from "./../../modules/download-file.js";
 import randomFromArray from "./../../modules/random-from-array.js";
 import getWeather from "./../../modules/get-weather.js";
+import getImageLuminosity from "./../../modules/get-image-luminosity.js";
 
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const botID = 'volcanoviews';
+const botID = "volcanoviews";
 
 const botScript = async () => {
   const mastodon = new mastodonClient({
@@ -22,14 +22,21 @@ const botScript = async () => {
   const webcam = randomFromArray(webcams);
   const filePath = `${__dirname}/../../temp/${botID}.jpg`;
   await downloadFile(webcam.url, filePath);
-  const status = `${webcam.name} via ${webcam.page_url} #volcano #nature`;
 
-  mastodon.postImage({
-    status,
-    image: filePath,
-    alt_text: "Webcam view of a volcano.",
-  });
-  return true;  
-}
+  const luminosity = await getImageLuminosity(filePath);
+
+  if (luminosity > 40) {
+    const status = `${webcam.name} via ${webcam.page_url} #volcano #nature`;
+
+    mastodon.postImage({
+      status,
+      image: filePath,
+      alt_text: "Webcam view of a volcano.",
+    });
+    return true;
+  } else {
+    return await botScript();
+  }
+};
 
 export default botScript;

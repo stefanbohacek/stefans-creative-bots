6266934102e261ba3de830dccf40cc41af1bd728
Stefan Bohacek <stefan@stefanbohacek.com>
2025-09-05 17:23:08 -0400

    Updated webcam links for ISS.

--- data/webcams/iss.js
+++ data/webcams/iss.js
@@ -1,16 +1,16 @@
 export default [
-  // {
-  //   description:
-  //     "Live video from the International Space Station, including inside views when the crew aboard the space station is on duty. Views of Earth are also streamed from an external camera located outside of the space station. During periods of signal loss due to handover between communications satellites, a blue screen is displayed.",
-  //   url: "https://www.youtube.com/watch?v=jKHvbJe9c_Y",
-  //   youtube_url: "https://www.youtube.com/watch?v=jKHvbJe9c_Y",
-  //   tags: "#iss #space #earth #nasa #esa #jaxa #csa",
-  // },
+  {
+    description:
+      "Live video from the International Space Station, including inside views when the crew aboard the space station is on duty. Views of Earth are also streamed from an external camera located outside of the space station. During periods of signal loss due to handover between communications satellites, a blue screen is displayed.",
+    url: "https://www.youtube.com/watch?v=iYmvCUonukw",
+    youtube_url: "https://www.youtube.com/watch?v=iYmvCUonukw",
+    tags: "#iss #space #earth #nasa #esa #jaxa #csa",
+  },
   {
     description:
       "Live views from the International Space Station are streaming from an external camera mounted on the station's Harmony module. The camera is looking forward at an angle so that International Docking Adapter 2 is visible. If the Harmony module camera is not available due to operational considerations for a longer period of time, a continuous loop of recorded Earth views will be displayed with the caption 'Previously Recorded'.",
-    url: "https://eol.jsc.nasa.gov/esrs/hdev/",
-    youtube_url: "https://www.youtube.com/watch?v=H999s0P1Er0",
+    url: "https://www.youtube.com/watch?v=sSabxiFMjKc",
+    youtube_url: "https://www.youtube.com/watch?v=sSabxiFMjKc",
     tags: "#iss #space #earth #nasa #esa #jaxa #csa",
   },
 ];

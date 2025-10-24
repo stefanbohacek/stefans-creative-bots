587153b2c53677018474300fd79db38d24c438e1
Stefan Bohacek <stefan@stefanbohacek.com>
2025-10-24 15:01:28 -0400

    Added statues bot.

--- test.js
+++ test.js
@@ -1,15 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-// import bot from "./bots/trains/bot.js";
-// import bot from './bots/wikipediatopedits/bot.js';
-// import bot from "./bots/ignoreallpreviousinstructions/bot.js";
 // import bot from "./bots/test/bot.js";
-// import bot from "./bots/linkedin-openings/bot.js";
-// import bot from "./bots/galaxy/bot.js";
-// import bot from "./bots/nycdatabot/bot.js";
 // import bot from "./bots/roots/bot.js";
-import bot from "./bots/birds/bot.js";
+// import bot from "./bots/birds/bot.js";
+import bot from "./bots/statues/bot.js";
 
 try {
   bot();

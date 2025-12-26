0704ca55c645e9f792fc1d65715a8af554b8900a
Stefan Bohacek <stefan@stefanbohacek.com>
2025-12-26 09:06:32 -0500

    Switched to Nameday API v2, minor cleanup.

--- bots/happynameday/bot.js
+++ bots/happynameday/bot.js
@@ -1,5 +1,5 @@
 import mastodonClient from "./../../modules/mastodon/index.js";
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 import pluralize from "pluralize";
 import oxfordComma from "./../../modules/oxford-comma.js";
 
@@ -7,38 +7,54 @@ import oxfordComma from "./../../modules/oxford-comma.js";
 // https://nameday.abalin.net/
 
 const botScript = async () => {
- await (async () => {
+  await (async () => {
     try {
       const mastodon = new mastodonClient({
         access_token: process.env.HAPPY_NAME_DAY_BOT_MASTODON_TOKEN,
         api_url: process.env.MASTODON_API_URL,
       });
 
-      const resp = await fetch("https://nameday.abalin.net/api/V1/today");
+      const resp = await fetch(
+        "https://nameday.abalin.net/api/V2/today/New_York"
+      );
       const respJSON = await resp.json();
-      
-      if (respJSON.nameday){
+
+      console.log(respJSON);
+
+      if (respJSON.data) {
+        const excluded = ["n/a", "2. svátek vánoční"];
         let nameDays = [];
-      
-        for (let country in respJSON.nameday){
-          nameDays.push(...respJSON.nameday[country].split(",").filter(name => name !== "n/a"));
+
+        for (let country in respJSON.data) {
+          if (country !== "ru") {
+            nameDays.push(
+              ...respJSON.data[country]
+                .split(",")
+                .filter((name) => !excluded.some((str) => name.includes(str)))
+            );
+          }
         }
 
         // nameDays = [...new Set(nameDays)].map(name => pluralize(name.trim()));
-        nameDays = [...new Set(nameDays.map(name => pluralize(name.trim())))];
+        // nameDays = [...new Set(nameDays.map((name) => pluralize(name.trim())))].sort();
 
+        nameDays = [
+          ...new Set(nameDays.map((name) => pluralize(name.trim()))),
+        ].sort((a, b) => a.localeCompare(b, "en", { sensitivity: "base" }));
 
-        const status = `Happy name day to all the ${oxfordComma(nameDays)}!\n\n#nameDay #HappyNameDay`
+        const status = `Happy name day to all the ${oxfordComma(
+          nameDays
+        )}!\n\n#nameDay #HappyNameDay`;
         // console.log(status);
 
         mastodon.post({
-          status
+          status,
         });
-      }      
+      }
     } catch (error) {
       console.log(`Happy Name Day bot: error`, error);
     }
   })();
-}
+};
 
 export default botScript;

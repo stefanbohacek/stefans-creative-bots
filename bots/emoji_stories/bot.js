5c5ea650e6b779edd53802042e417420864aad01
Stefan Bohacek <stefan@stefanbohacek.com>
2026-05-25 18:41:17 -0400

    Switched the Emoji Stories bot to the updated emoji list.

--- bots/emoji_stories/bot.js
+++ bots/emoji_stories/bot.js
@@ -1,18 +1,35 @@
 ﻿import mastodonClient from "./../../modules/mastodon/index.js";
 import getRandomInt from "./../../modules/getRandomInt.js";
 import randomFromArray from "./../../modules/randomFromArray.js";
-import emoji from "./../../data/emoji.js";
+import emojiList from "./../../data/emoji-list.json" with { type: "json" };
 
 const botScript = async () => {
   const mastodon = new mastodonClient({
+    // access_token: process.env.MASTODON_TEST_TOKEN,
     access_token: process.env.EMOJI_STORIES_MASTODON_ACCESS_TOKEN,
     api_url: process.env.MASTODON_API_URL,
   });
 
-  const randomEmoji = randomFromArray(emoji, getRandomInt(4,6));
-  const status = randomEmoji.join(" ") + " \n\n#emoji #WritingPrompt #EmojiStories";
+  const prompts = [
+    "Write a short story based on these emoji:",
+    "Come up with a story based on these emoji:",
+    "Can you write a story based on these emoji?",
+  ];
+
+  const selectedPrompt = randomFromArray(prompts);
+  const selectedEmoji = randomFromArray(emojiList, getRandomInt(4, 6));
+  const selectedEmojiLines = selectedEmoji
+    .map((item) => `${item.emoji}: ${item.name}`)
+    .join("\n");
+
+  const status =
+    selectedPrompt +
+    "\n\n" +
+    selectedEmojiLines +
+    "\n\n#emoji #WritingPrompt #EmojiStories";
+
   mastodon.post({
-    status
+    status,
   });
 };
 

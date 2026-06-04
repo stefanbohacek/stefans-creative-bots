b07ef8fee5334f0a46ba962a33cb15fb1b81fcf2
Stefan Bohacek <stefan@stefanbohacek.com>
2026-06-04 10:15:00 -0400

    Switched to ET time zone.

--- modules/cronSchedules.js
+++ modules/cronSchedules.js
@@ -1,6 +1,3 @@
-// time is in UTC, subtract 5 hours for EST
-// '0 8 * * *' would be every day at 8-5 = 3am
-
 export default {
   EVERY_SECOND: '*/1 * * * * *',
   EVERY_FIVE_SECONDS: '*/5 * * * * *',
@@ -24,10 +21,10 @@ export default {
   EVERY_ELEVEN_HOURS: '15 */11 * * *',
   EVERY_TWELVE_HOURS: '1 */12 * * *',
   EVERY_DAY_MIDNIGHT: '0 0 * * *',
-  EVERY_DAY_MORNING: '0 10 * * *',
-  EVERY_DAY_LATE_MORNING: '0 14 * * *',
-  EVERY_DAY_NOON: '0 17 * * *',
-  EVERY_DAY_AFTERNOON: '0 19 * * *',
-  EVERY_DAY_EVENING: '0 0 * * *',
+  EVERY_DAY_MORNING: '0 6 * * *',
+  EVERY_DAY_LATE_MORNING: '0 10 * * *',
+  EVERY_DAY_NOON: '0 12 * * *',
+  EVERY_DAY_AFTERNOON: '0 14 * * *',
+  EVERY_DAY_EVENING: '0 19 * * *',
   EVERY_WEEK: '0 10 * * 1',
 };

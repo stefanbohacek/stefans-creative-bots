06dd51e8bc5e1662e557df88a2b7976e1aec9437
Stefan Bohacek <stefan@stefanbohacek.com>
2026-06-06 13:48:51 -0400

    Code cleanup.

--- modules/scheduleBot.js
+++ modules/scheduleBot.js
@@ -16,19 +16,19 @@ export default async (bot, app) => {
 
         mastodonStream.on("message", async (message) => {
           try {
-            console.log("received message...", {
-              event: message.event,
-              id: message.data.id,
-              created_at: message.data.created_at,
-              in_reply_to_id: message.data?.status?.in_reply_to_id,
-              in_reply_to_account_id: message.data?.status?.in_reply_to_account_id,
-              visibility: message.data?.status?.visibility,
-              content: message.data?.status?.content || message.data?.content || "",
-              mentions: message.data?.status?.mentions,
-              account_id: message.data.account?.id,
-              account_username: message.data.account?.username,
-              account_display_name: message.data.account?.display_name,
-            });
+            // console.log("received message...", {
+            //   event: message.event,
+            //   id: message.data.id,
+            //   created_at: message.data.created_at,
+            //   in_reply_to_id: message.data?.status?.in_reply_to_id,
+            //   in_reply_to_account_id: message.data?.status?.in_reply_to_account_id,
+            //   visibility: message.data?.status?.visibility,
+            //   content: message.data?.status?.content || message.data?.content || "",
+            //   mentions: message.data?.status?.mentions,
+            //   account_id: message.data.account?.id,
+            //   account_username: message.data.account?.username,
+            //   account_display_name: message.data.account?.display_name,
+            // });
             if (
               (message.event === "notification" && message.data.type === "mention") ||
               message.event === "update"

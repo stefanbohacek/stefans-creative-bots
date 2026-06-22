7d1bd188ca82e528c61ad1b32a6a1777c96b596a
Stefan Bohacek <stefan@stefanbohacek.com>
2026-06-22 12:18:54 -0400

    Switched to live token.

--- bots/njdatabot/bot.js
+++ bots/njdatabot/bot.js
@@ -50,8 +50,8 @@ const getLongLat = (datapoint) => {
 };
 
 const mastodon = new mastodonClient({
-  // access_token: process.env.NJ_DATA_BOT_MASTODON_ACCESS_TOKEN_SECRET,
-  access_token: process.env.MASTODON_TEST_TOKEN,
+  access_token: process.env.NJ_DATA_BOT_MASTODON_ACCESS_TOKEN_SECRET,
+  // access_token: process.env.MASTODON_TEST_TOKEN,
   api_url: process.env.MASTODON_API_URL,
 });
 
